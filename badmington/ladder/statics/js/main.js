abfb2efbc82e6d30e1c69cdfbd4ce05aacb4fa2c
Romain Gisclard <romain.gisclard@ifsalpha.com>
2018-11-15 15:40:36 +0100

    player list

--- badmington/ladder/statics/js/main.js
+++ badmington/ladder/statics/js/main.js
@@ -1,1 +1,12 @@
 const result = () => window.prompt('inscriver votre resultat (v = victoire / D = defaite)')
+const send = () => {
+    const xhttp = new XMLHttpRequest();
+    const name
+    xhttp.onreadystatechange = function () {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        name = document.getElementById("name").innerHTML = xhttp.responseText;
+      }
+    };
+    xhttp.open("GET", "/new/" + name, true);
+    xhttp.send();
+  }
